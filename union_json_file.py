e5ea6a0f08dea3f023e5b8a41d2be1c8994f5685
Michail27 <michail27.03@mail.ru>
2021-12-27 10:26:04 +0300

    refactoring get_result_dict

--- union_json_file.py
+++ union_json_file.py
@@ -9,13 +9,10 @@ class AbstractUnion(ABC):
 
 class JsonUnion(AbstractUnion):
     def get_result_dict(self, students_dict, rooms_dict):
+        dict_rooms = dict()
         for room in rooms_dict:
             room['students'] = []
-        dict_rooms = {room['id']: room for room in rooms_dict}
+            dict_rooms[room['id']] = room
         for student in students_dict:
-            student_room = student['room']
-            student_dict = dict()
-            student_dict['id'] = student['id']
-            student_dict['name'] = student['name']
-            dict_rooms[student_room]['students'].append(student_dict)
+            dict_rooms[student['room']]['students'].append({'id': student['id'], 'name': student['name']})
         return [*dict_rooms.values()]
